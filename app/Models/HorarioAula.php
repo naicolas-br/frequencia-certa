f9028763bc7e67413595fdb5e49f193d5eae987a
Nicolas <nicolas.viana.moc@gmail.com>
2025-12-18 12:29:11 -0300

    feat(models): adicionar model HorarioAula com fillable e relacionamento com Disciplina

--- app/Models/HorarioAula.php
+++ app/Models/HorarioAula.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class HorarioAula extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * IMPORTANTE:
+     * O Laravel tentaria buscar a tabela 'horario_aulas' (plural do nome da classe).
+     * Como nossa migration criou 'grade_horarias', precisamos forçar esse nome aqui.
+     */
+    protected $table = 'grade_horarias';
+
+    /**
+     * O erro 'MassAssignmentException' aconteceu porque não listamos
+     * aqui quais campos são seguros para serem salvos.
+     */
+    protected $fillable = [
+        'user_id',       // Adicionei user_id caso precisemos filtrar por aluno direto
+        'disciplina_id',
+        'dia_semana',
+        'horario_inicio',
+        'horario_fim'
+    ];
+
+    // Relacionamento: Um horário pertence a uma Disciplina
+    public function disciplina()
+    {
+        return $this->belongsTo(Disciplina::class);
+    }
 }
