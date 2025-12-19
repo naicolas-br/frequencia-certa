cd7329ca1f3655c92e373579a142e18bea257f1f
Nicolas <nicolas.viana.moc@gmail.com>
2025-12-19 13:34:38 -0300

    chore(i18n): traduz mensagens de erro do Laravel Breeze

---
+++ resources/lang/pt_BR/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed'   => 'E-mail ou senha incorretos',
+    'password' => 'A senha está errada.',
+    'throttle' => 'O número limite de tentativas de login foi atingido. Por favor, tente novamente dentro de :seconds segundos.',
+];
