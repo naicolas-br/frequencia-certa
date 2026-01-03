cf8ac6cd51510d0c2b590e1034cd77baf9bff3f7
Nicolas <nicolas.viana.moc@gmail.com>
2026-01-03 05:45:41 -0300

    chore(mail): personalizar layout e identidade visual do e-mail

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+             <img src="https://frequencia-certa.onrender.com/logo.png" class="logo" alt="Frequência Certa" style="max-height: 70px;">
+        </a>
+    </td>
+</tr>
